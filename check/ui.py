3dd6893d37c2ab8361209ed2dfcc9742c573b79b
huangchenghan <hermit515291@gmail.com>
2022-06-07 23:30:49 +0800

    add employee combobox
    add employee combobox

--- check/ui.py
+++ check/ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'uiaVhYaZ.ui'
+## Form generated from reading UI file 'uiePTOPm.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -27,30 +27,30 @@ class Ui_MainWindow(object):
         self.widget.setObjectName(u"widget")
         self.label_8 = QLabel(self.widget)
         self.label_8.setObjectName(u"label_8")
-        self.label_8.setGeometry(QRect(10, 10, 171, 30))
+        self.label_8.setGeometry(QRect(10, 60, 171, 30))
         self.label_8.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 "font: 75 16pt \"Rockwell\";\n"
 "background-color: rgb(0, 0, 0);")
         self.search_tableWidget = QTableWidget(self.widget)
         self.search_tableWidget.setObjectName(u"search_tableWidget")
-        self.search_tableWidget.setGeometry(QRect(10, 50, 750, 278))
+        self.search_tableWidget.setGeometry(QRect(10, 100, 750, 278))
         self.search_tableWidget.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 "background-color: rgb(60, 60, 60);")
         self.label_6 = QLabel(self.widget)
         self.label_6.setObjectName(u"label_6")
-        self.label_6.setGeometry(QRect(10, 370, 120, 30))
+        self.label_6.setGeometry(QRect(10, 390, 120, 30))
         self.label_6.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 "background-color: rgb(0, 0, 0);\n"
 "font: 12pt \"Rockwell\"; ")
         self.product_label = QLabel(self.widget)
         self.product_label.setObjectName(u"product_label")
-        self.product_label.setGeometry(QRect(10, 400, 120, 30))
+        self.product_label.setGeometry(QRect(10, 420, 120, 30))
         self.product_label.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 "background-color: rgb(60, 60, 60);\n"
 "font: 10pt \"Rockwell\"; ")
         self.accept_pushButton = QPushButton(self.widget)
         self.accept_pushButton.setObjectName(u"accept_pushButton")
-        self.accept_pushButton.setGeometry(QRect(500, 400, 120, 30))
+        self.accept_pushButton.setGeometry(QRect(500, 420, 120, 30))
         self.accept_pushButton.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 " font: 10pt \"Rockwell\"; \n"
 "background-color: rgb(60, 60, 60); \n"
@@ -61,7 +61,7 @@ class Ui_MainWindow(object):
 "padding: 2 2px 2 2px;")
         self.reject_pushButton = QPushButton(self.widget)
         self.reject_pushButton.setObjectName(u"reject_pushButton")
-        self.reject_pushButton.setGeometry(QRect(630, 400, 120, 30))
+        self.reject_pushButton.setGeometry(QRect(630, 420, 120, 30))
         self.reject_pushButton.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 " font: 10pt \"Rockwell\"; \n"
 "background-color: rgb(60, 60, 60); \n"
@@ -72,30 +72,42 @@ class Ui_MainWindow(object):
 "padding: 2 2px 2 2px;")
         self.price_lineEdit = QLineEdit(self.widget)
         self.price_lineEdit.setObjectName(u"price_lineEdit")
-        self.price_lineEdit.setGeometry(QRect(150, 400, 90, 30))
+        self.price_lineEdit.setGeometry(QRect(150, 420, 90, 30))
         self.price_lineEdit.setStyleSheet(u"color: rgb(255, 255, 255);")
         self.state_lineEdit = QLineEdit(self.widget)
         self.state_lineEdit.setObjectName(u"state_lineEdit")
-        self.state_lineEdit.setGeometry(QRect(250, 400, 90, 30))
+        self.state_lineEdit.setGeometry(QRect(250, 420, 90, 30))
         self.state_lineEdit.setStyleSheet(u"color: rgb(255, 255, 255);")
         self.label = QLabel(self.widget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(150, 370, 120, 30))
+        self.label.setGeometry(QRect(150, 390, 120, 30))
         self.label.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 "background-color: rgb(0, 0, 0);\n"
 "font: 12pt \"Rockwell\"; ")
         self.label_2 = QLabel(self.widget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(250, 370, 58, 30))
+        self.label_2.setGeometry(QRect(250, 390, 58, 30))
         self.label_2.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 "background-color: rgb(0, 0, 0);\n"
 "font: 12pt \"Rockwell\"; ")
         self.class_comboBox = QComboBox(self.widget)
         self.class_comboBox.setObjectName(u"class_comboBox")
-        self.class_comboBox.setGeometry(QRect(350, 400, 100, 30))
+        self.class_comboBox.setGeometry(QRect(350, 420, 100, 30))
         self.class_comboBox.setStyleSheet(u"color: rgb(255, 255, 255);\n"
 "background-color: rgb(60, 60, 60);\n"
 "font: 10pt \"Rockwell\"; ")
+        self.comboBox = QComboBox(self.widget)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QRect(360, 20, 120, 30))
+        self.comboBox.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"background-color: rgb(60, 60, 60);\n"
+"font: 10pt \"Rockwell\"; ")
+        self.label_3 = QLabel(self.widget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(250, 20, 120, 30))
+        self.label_3.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"font: 75 12pt \"Rockwell\";\n"
+"background-color: rgb(0, 0, 0);")
 
         self.horizontalLayout.addWidget(self.widget)
 
@@ -115,5 +127,6 @@ class Ui_MainWindow(object):
         self.reject_pushButton.setText(QCoreApplication.translate("MainWindow", u"Reject", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Price :", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"State :", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Employee :", None))
     # retranslateUi
 
